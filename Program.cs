a3372275bdd55744b37033f61cf1982c143991f6
San4eeez <AlexandroHell@yandex.ru>
2023-10-10 14:44:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string hello1 = @"
+                                                                                           
+MMMMMMMM               MMMMMMMM                                  MMMMMMMM               MMMMMMMM
+M:::::::M             M:::::::M                                  M:::::::M             M:::::::M
+M::::::::M           M::::::::M                                  M::::::::M           M::::::::M
+M:::::::::M         M:::::::::M                                  M:::::::::M         M:::::::::M
+M::::::::::M       M::::::::::M        mmmmmmm    mmmmmmm        M::::::::::M       M::::::::::M
+M:::::::::::M     M:::::::::::M      mm:::::::m  m:::::::mm      M:::::::::::M     M:::::::::::M
+M:::::::M::::M   M::::M:::::::M     m::::::::::mm::::::::::m     M:::::::M::::M   M::::M:::::::M
+M::::::M M::::M M::::M M::::::M     m::::::::::::::::::::::m     M::::::M M::::M M::::M M::::::M
+M::::::M  M::::M::::M  M::::::M     m:::::mmm::::::mmm:::::m     M::::::M  M::::M::::M  M::::::M
+M::::::M   M:::::::M   M::::::M     m::::m   m::::m   m::::m     M::::::M   M:::::::M   M::::::M
+M::::::M    M:::::M    M::::::M     m::::m   m::::m   m::::m     M::::::M    M:::::M    M::::::M
+M::::::M     MMMMM     M::::::M     m::::m   m::::m   m::::m     M::::::M     MMMMM     M::::::M
+M::::::M               M::::::M     m::::m   m::::m   m::::m     M::::::M               M::::::M
+M::::::M               M::::::M     m::::m   m::::m   m::::m     M::::::M               M::::::M
+M::::::M               M::::::M     m::::m   m::::m   m::::m     M::::::M               M::::::M
+MMMMMMMM               MMMMMMMM     mmmmmm   mmmmmm   mmmmmm     MMMMMMMM               MMMMMMMM                                                                                                                                                                                                                                                                                      
+";
+Console.WriteLine(hello1 + "\nДобро пожаловать в банк МММ, Сергей Пантелеевич приветствует вас!\n\n ");
+
+int shetNumber = 3444444;
+string fio = "Мавроди Сергей Пантелеевич";
+float balance = 99999;
+
+while (true)
+{
+    Console.WriteLine("Что вы хоите сделать?\n1 - открыть счёт\n2 - показать информацию о счёте\n3 - положить деньги на счёт\n4 - снять со счёта\n5 - Обналичить счёт полностью\n6 - Перевести с одного счёта на другой");
+    Console.Write("Ввод: ");
+    Console.ReadLine();
+
+   
+
+
+}
+
+class Bank
+{
+
+}
