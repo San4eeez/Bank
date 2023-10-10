5d51a95291be89d69235f035cd6c99a5530b586c
San4eeez <AlexandroHell@yandex.ru>
2023-10-10 16:58:21 +0300

    Начал делать

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿string hello1 = @"
+﻿using BankMetod;
+/*Bank bank1 = new Bank();
+Bank bank2 = new Bank();
+*/
+int shetNumber = 3444444;
+string fio = "Мавроди Сергей Пантелеевич";
+float balance = 99999;
+
+Bank[] Schet = new Bank[2];
+Schet[0] = new Bank();
+Schet[1] = new Bank();
+
+int shetNumber2 = 6666;
+string fio2 = "Апрашкин Валерий Мамадзиёвович";
+float balance2 = 55;
+
+Schet[0].otk(shetNumber, fio,balance);
+Schet[1].otk(shetNumber2, fio2, balance2);
+
+
+
+
+for (int i = 0; i < Schet.Length; i++)
+{
+    Schet[i].Out();
+}
+
+string hello1 = @"
                                                                                            
 MMMMMMMM               MMMMMMMM                                  MMMMMMMM               MMMMMMMM
 M:::::::M             M:::::::M                                  M:::::::M             M:::::::M
@@ -19,11 +46,38 @@ MMMMMMMM               MMMMMMMM     mmmmmm   mmmmmm   mmmmmm     MMMMMMMM       
 ";
 Console.WriteLine(hello1 + "\nДобро пожаловать в банк МММ, Сергей Пантелеевич приветствует вас!\n\n ");
 
-int shetNumber = 3444444;
-string fio = "Мавроди Сергей Пантелеевич";
-float balance = 99999;
 
-while (true)
+
+/*bank1.otk(shetNumber, fio, balance);
+bank1.Out();
+
+bank2.otk(shetNumber2, fio2, balance2);
+bank2.Out();
+
+Console.WriteLine("Пополнить счёт.");
+Console.Write("Введите сумму: ");
+float pmoney = float.Parse(Console.ReadLine());
+bank1.Dob(pmoney);
+bank1.Out();
+
+Console.Write("Снять деньги со счёта.\nВведите сумму: ");
+float smoney = float.Parse(Console.ReadLine());
+bank1.Umen(smoney);
+bank1.Out();
+
+Console.Write("Обаличить счёт полностью.");
+bank1.Obnul();
+bank1.Out();
+
+Console.Write("Перевод с счёта на счёт.\nВведите сумму: ");
+float permpney = float.Parse(Console.ReadLine());
+bank1.Umen(permpney);
+bank2.Dob(pmoney);
+bank1.Out();
+bank2.Out();*/
+
+
+/*while (true)
 {
     Console.WriteLine("Что вы хоите сделать?\n1 - открыть счёт\n2 - показать информацию о счёте\n3 - положить деньги на счёт\n4 - снять со счёта\n5 - Обналичить счёт полностью\n6 - Перевести с одного счёта на другой");
     Console.Write("Ввод: ");
@@ -33,4 +87,4 @@ while (true)
 
 
 }
-
+*/
--- bank.cs
+++ bank.cs
@@ -12,12 +12,57 @@ namespace BankMetod
         private string name;
         private float sum;
 
-       
+
+
+        public void otk(int nom,string name,float sum)
+        {
+            this.nom = nom;
+            this.name = name;
+            this.sum = sum;
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"Ваш номер: {nom},Ваше имя: {name}, Сумма: {sum}");
+        }
+
+        public void Dob(float money)
+        {
+            Console.WriteLine("Вы успешно добавили на счёт "+money);
+            sum += money;
+        }
+
+        public void Umen (float money)
+        {
+            Console.WriteLine("Вы успешно сняли со счёта" +money);
+            sum -= money;
+        }
+
+        public void Obnul() 
+        {
+            Console.WriteLine("Вы успешно сняли со счёта всю сумму.\nС вашего счёта списано "+sum);
+            sum = 0;
+        }
+
+        public void perewod (float money)
+        {
+            Console.WriteLine($"Вы успешно перевели {money} на другой счёт");
+
+        }
 
 
 
 
     }
+
+
+
+
+
+
+
+
+
 
 
 }
